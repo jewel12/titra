ddf1ee136e884065ec2d9d0f14e5191c0d9fd8bb
jewelve <xiedajeeling@gmail.com>
2012-12-30 14:11:41 +0900

    Translationモデルの名前が間違っていたのでpadrino g model -d Trnaslationで関連ファイルを削除

--- models/trnaslation.rb
+++
@@ -1,3 +0,0 @@
-class Trnaslation < ActiveRecord::Base
-
-end
--- spec/models/trnaslation_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "Trnaslation Model" do
-  let(:trnaslation) { Trnaslation.new }
-  it 'can be created' do
-    trnaslation.should_not be_nil
-  end
-end
