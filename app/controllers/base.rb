69a914abac5f36661a3a3fad15807caccd12beb1
jewelve <xiedajeeling@gmail.com>
2012-12-31 17:15:46 +0900

    OmniauthでGithubとTwitterのアカウントを使ってログインできるようにした

---
+++ app/controllers/base.rb
@@ -0,0 +1,9 @@
+Titra.controllers do
+  get :index, :map => '/' do
+    haml <<-HAML
+Login with
+=link_to('Github', '/auth/github')
+=link_to('Twitter', '/auth/twitter')
+HAML
+  end
+end
--- models/account.rb
+++ models/account.rb
@@ -1,7 +1,16 @@
+require "yaml"
 class Account < ActiveRecord::Base
   has_many :title_translation
 
   [:name, :uid, :provider].each do |param|
     validates param, :presence => true, :space => true
+  end
+
+  def self.create_with_omniauth(auth)
+    create! do |account|
+      account.name = auth["info"].nickname
+      account.provider = auth["provider"]
+      account.uid = auth["uid"]
+    end
   end
 end
