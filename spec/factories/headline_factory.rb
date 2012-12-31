56d678a9a5e0e62bca8056f15b35aeadbaa28b4e
jewelve <xiedajeeling@gmail.com>
2012-12-31 11:47:17 +0900

    Headline FactoryのURL生成をsequenceに

--- spec/factories/headline_factory.rb
+++ spec/factories/headline_factory.rb
@@ -2,10 +2,9 @@ FactoryGirl.define do
   factory :headline do
     title "Ruby is good!"
     is_translated false
-    url "http://ruby-example1.com/"
+    sequence(:url) { |i| "http://ruby-example#{i}.com/" }
 
     factory :headline_with_translations do
-      url "http://ruby-example2.com/"
       title_translations {FactoryGirl.create_list(:title_translation, 2)}
     end
   end
