fb4b4bbaf72f4e11b1d6e4bb1c63c4012d6a231c
jewelve <xiedajeeling@gmail.com>
2013-01-01 09:26:10 +0900

    翻訳者と紐付けて保存するロジックをTitleTranslationモデル側に移動

--- app/controllers/title_translation_controller.rb
+++ app/controllers/title_translation_controller.rb
@@ -1,12 +1,10 @@
 Titra.controllers :translations do
   post :new do
     redirect url(:headlines, :index) unless logged_in?
+
     tr_params = params["title_translation"]
-    @translation = TitleTranslation.new( :title => tr_params["title"] )
-    @translation.headline_id = tr_params["headline_id"].to_i
-    @translation.translator = current_account
-    @translation.save!
-    warn @translation.translator
+    @translation = TitleTranslation.create_with_translator(current_account, tr_params)
+
     redirect url(:headlines, :index)
   end
 end
