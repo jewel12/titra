094df11dc7fa052c5a293f05d2b72387c8fb447f
jewelve <xiedajeeling@gmail.com>
2013-01-01 16:00:11 +0900

    TitleTranslation追加時に翻訳者とヘッドラインの組み合わせが既に存在していれば更新のみを行う

--- app/controllers/title_translation_controller.rb
+++ app/controllers/title_translation_controller.rb
@@ -3,7 +3,11 @@ Titra.controllers :translations do
     redirect url(:headlines, :index) unless logged_in?
 
     tr_params = params["title_translation"]
-    @translation = TitleTranslation.create_with_translator(current_account, tr_params)
+    tr_params[:translator] = current_account
+    tr_params[:headline] = Headline.find(tr_params[:headline_id])
+    # @translation = TitleTranslation.create_with_translator(current_account, tr_params)
+
+    @translation = TitleTranslation.create_or_update(tr_params)
 
     redirect url(:headlines, :index)
   end
