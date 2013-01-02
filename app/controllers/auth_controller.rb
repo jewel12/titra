a1aa6789cbc187f9187dd60c729a7ec1dfa544d6
jewelve <xiedajeeling@gmail.com>
2013-01-02 10:46:44 +0900

    ログアウトできるようにした

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,6 +4,11 @@ Titra.controllers :auth do
     render 'auth/login'
   end
 
+  get :logout do
+    set_current_account(nil)
+    redirect url_for(:headlines, :index)
+  end
+
   get :auth, :map => '/auth/:provider/callback' do
     auth = request.env["omniauth.auth"]
     account = Account.find_by_provider_and_uid(auth["provider"], auth["uid"]) ||
