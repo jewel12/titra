13902d9f81df5c4acb090d082f0aaf83b5cf5722
jewelve <xiedajeeling@gmail.com>
2013-03-13 13:43:05 +0900

    Added 要約の１行要約を表示させるようにした

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,9 +2,13 @@ $(document).ready(function(){
     $('.translation').each(function() {
         var translation_tag = $(this);
         var summary = translation_tag.find('p.summary');
+        var summary_summary = translation_tag.find('span.summary_summary');
 
         summary.hide();
 
-        translation_tag.click( function(){summary.toggle();} );
+        translation_tag.click( function(){
+            summary.toggle();
+            summary_summary.toggle();
+        } );
     });
 });
